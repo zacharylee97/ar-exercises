f33c594b09a3aa7dd5bf0e499845b5997342bda0
Zachary Lee <zacharylhlee@gmail.com>
2018-11-06 17:59:00 +0000

    Displays error messages when attempting to create record

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please input store name"
+print "> "
+@store_name = gets.chomp
+new_store = Store.create(name: "#{@store_name}")
+puts new_store.errors.full_messages
