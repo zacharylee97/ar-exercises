83c1473f00800a25b3a4a75d6b295ef868a097ea
Zachary Lee <zacharylhlee@gmail.com>
2018-11-06 17:05:29 +0000

    Loads mens stores and womens stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,15 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Surrey = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Whistler = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Yaletown = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+@womens_stores.each do |store|
+  puts "#{store.name}: #{store.annual_revenue}"
+end
