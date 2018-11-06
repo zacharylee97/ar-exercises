ebda8c0557f1e62e71882b6d76feee1bf98e19da
Zachary Lee <zacharylhlee@gmail.com>
2018-11-06 17:11:24 +0000

    Outputs total revenue, average revenue and number of stores generating M or more

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts Store.sum(:annual_revenue)
+puts Store.sum(:annual_revenue) / Store.count
+puts Store.where("annual_revenue >= ?", 1000000).count
