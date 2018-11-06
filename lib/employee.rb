b6b7fa834434781f1122ad975dd5316c5f6f6353
Zachary Lee <zacharylhlee@gmail.com>
2018-11-06 17:27:25 +0000

    Adds validations for employees

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,8 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, inclusion: { in: (40..200) }
+  validates :store_id, presence: true
+
 end
